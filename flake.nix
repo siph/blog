ac2ec459f48a02da91ee4394fadd04c30cc164c2
siph <dawkins.chris.dev@gmail.com>
2023-03-09 16:35:11 -0700

    bundix

--- flake.nix
+++ flake.nix
@@ -12,9 +12,18 @@
           pkgs = import nixpkgs {
             inherit system;
           };
+          gems = pkgs.bundlerEnv {
+            name = "gems";
+            ruby = pkgs.ruby;
+            gemfile = ./Gemfile;
+            lockfile = ./Gemfile.lock;
+            gemset = ./gemset.nix;
+          };
           buildInputs = with pkgs; [
             jekyll
             bundler
+            bundix
+            gems
           ];
         in {
           devShell = with pkgs;
