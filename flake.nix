2ff317fff55928ccbc875ee10ce5c38b0eb100b9
siph <dawkins.chris.dev@gmail.com>
2023-03-08 18:36:32 -0700

    nix init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Blog";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          buildInputs = with pkgs; [
+            jekyll
+          ];
+        in {
+          devShell = with pkgs;
+            mkShell {
+              inherit buildInputs;
+            };
+        });
+}
+
